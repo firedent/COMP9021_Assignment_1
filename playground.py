ea334f0a30747cd3d830b6cde67a46b1f40ca979
zscself <zscself@gmail.com>
2018-08-22 23:30:18 +1000

    更新测试用例

--- playground.py
+++ playground.py
@@ -1,12 +1,4 @@
 # coding=utf-8
-import sys
-
-# 读取文件的
-with open('monthly_csv.csv') as f:
-    lines = f.readlines()[1:5]
-
-print(lines)
-lines_p = []
-for i in lines:
-    lines_p.append(i.replace('\n', ''))
-
+import os
+if not os.path.exists('land.txt'):
+    print('sss')
